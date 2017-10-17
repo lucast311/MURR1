5cab14fee6191e167f3db1d91e309d409bc5f6ef
Matt Weeks <weeks8056@saskpolytech.ca>
2017-10-17 09:02:54 -0600

    Finished most unit tests for story - Matt Weeks

--- src/AppBundle/Controller/CommunicationController.php
+++ src/AppBundle/Controller/CommunicationController.php
@@ -1,17 +1,27 @@
-<?
+<?php
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/communication", name="communication")
+ */
 class CommunicationController extends Controller
 {
+    public function indexAction(Request $request)
+    {
+
+    }
+
     /**
      * @Route("/communication/submit", name="communication_submit")
      */
     public function submitAction(Request $request)
     {
-
+        return $this->render('default/index.html.twig', [
+    'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+]);
     }
 }
--- tests/AppBundle/Controller/CommunicationControllerTest.php
+++ tests/AppBundle/Controller/CommunicationControllerTest.php
@@ -3,8 +3,9 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+//use Doctrine\Common\Persistence\ObjectRepository;
 
-class DefaultControllerTest extends WebTestCase
+class CommunicationControllerTest extends WebTestCase
 {
     public function testFormSuccess()
     {
@@ -15,17 +16,17 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['date'] = "2017-10-05";
-        $form['type']="Phone";
-        $form['medium']="incoming";
-        $form['contact']=1; //contact id
-        $form['property']=1; //property id
-        $form['category']="Container";
-        $form["description"]="Container has graffiti and needs to be cleaned. Action request made";
+        $form['form[date]'] = "2017-10-05";
+        $form['form[type]']="Phone";
+        $form['form[medium]']="incoming";
+        $form['form[contact]']=1; //contact id
+        $form['form[property]']=1; //property id
+        $form['form[category]']="Container";
+        $form["form[description]"]="Container has graffiti and needs to be cleaned. Action request made";
 
         $crawler = $client->submit($form);
 
-        $this->assertContains("Communication added successfully",$crawler->filter('.success')->text());
+        $this->assertContains("Communication added successfully",$client->getResponse()->getContent());
     }
 
     public function testFutureDate()
@@ -38,11 +39,11 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['date'] = "2019-10-05";
+        $form['form[date]'] = "2019-10-05";
 
         $crawler = $client->submit($form);
 
-        assertContains("Please select a current or past date",$crawler->filter('.error')->text());
+        assertContains("Please select a current or past date",$client->getResponse()->getContent());
     }
 
     public function testEmptyDate()
@@ -54,11 +55,11 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['date'] = "";
+        $form['form[date]'] = "";
 
         $crawler = $client->submit($form);
 
-        assertContains("Please select a date",$crawler->filter('.error')->text());
+        assertContains("Please select a date",$client->getResponse()->getContent());
     }
 
     public function testNonExistantDate()
@@ -70,11 +71,11 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['date'] = "2017-2-30";
+        $form['form[date]'] = "2017-2-30";
 
         $crawler = $client->submit($form);
 
-        assertContains("Please select a valid date",$crawler->filter('.error')->text());
+        assertContains("Please select a valid date",$client->getResponse()->getContent());
     }
 
     public function testNoType()
@@ -86,11 +87,11 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['type']=0;
+        $form['form[type]']=0;
 
         $crawler = $client->submit($form);
 
-        assertContains("Please select a type of communication",$crawler->filter('.error')->text());
+        assertContains("Please select a type of communication",$client->getResponse()->getContent());
     }
 
     public function testNoMedium()
@@ -102,11 +103,11 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['medium']=0;
+        $form['form[medium]']=0;
 
         $crawler = $client->submit($form);
 
-        assertContains("Please select incoming or outgoing",$crawler->filter('.error')->text());
+        assertContains("Please select incoming or outgoing",$client->getResponse()->getContent());
     }
 
     public function testBlankContact()
@@ -118,11 +119,11 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['contact']=0; //blank contact ID
+        $form['form[contact]']=0; //blank contact ID
 
         $crawler = $client->submit($form);
 
-        assertContains("Please enter a contact",$crawler->filter('.error')->text());
+        assertContains("Please enter a contact",$client->getResponse()->getContent());
     }
 
     public function testResidentContact()
@@ -134,17 +135,17 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['date'] = "2017-10-05";
-        $form['type']="Phone";
-        $form['medium']="incoming";
-        $form['contact']=1; //contact id
-        $form['property']=-1; //identifier for a resident, will not be stored
-        $form['category']="Container";
-        $form["description"]="Container has graffiti and needs to be cleaned. Action request made";
+        $form['form[date]'] = "2017-10-05";
+        $form['form[type]']="Phone";
+        $form['form[medium]']="incoming";
+        $form['form[contact]']=-1; //identifier for a resident, will not be stored
+        $form['form[property]']=1;
+        $form['form[category]']="Container";
+        $form["form[description]"]="Container has graffiti and needs to be cleaned. Action request made";
 
         $crawler = $client->submit($form);
 
-        $this->assertContains("Communication added successfully",$crawler->filter('.success')->text());
+        $this->assertContains("Communication added successfully",$client->getResponse()->getContent());
     }
 
     public function testBlankProperty()
@@ -156,10 +157,129 @@ class DefaultControllerTest extends WebTestCase
         $form = $crawler->selectButton('submit')->form();
 
         //set form values
-        $form['property']=0; //blank property ID
+        $form['form[property]']=0; //blank property ID
 
         $crawler = $client->submit($form);
 
-        assertContains("Please select a property",$crawler->filter('.error')->text());
+        assertContains("Please select a property",$client->getResponse()->getContent());
     }
+
+    public function testMultiOrNAProperty()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/communication/submit');
+
+        $form = $crawler->selectButton('submit')->form();
+
+        //set form values
+        //set form values
+        $form['form[date]'] = "2017-10-05";
+        $form['form[type]']="Phone";
+        $form['form[medium]']="incoming";
+        $form['form[contact]']=1; //contact id
+        $form['form[property]']=-1; //multi-property or N/A property identifier
+        $form['form[category]']="Container";
+        $form["form[description]"]="Container has graffiti and needs to be cleaned. Action request made";
+
+        $crawler = $client->submit($form);
+
+        assertContains("Communication added successfully",$client->getResponse()->getContent());
+    }
+
+    public function testBlankCategory()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/communication/submit');
+
+        $form = $crawler->selectButton('submit')->form();
+
+        //set form values
+        $form['form[category]']=0; //blank category value
+
+
+        $crawler = $client->submit($form);
+
+        assertContains("Please select a category",$client->getResponse()->getContent());
+    }
+
+    public function testBlankDescription()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/communication/submit');
+
+        $form = $crawler->selectButton('submit')->form();
+
+        //set form values
+        $form['form[description]']=""; //blank description
+
+
+        $crawler = $client->submit($form);
+
+        assertContains("Please provide a brief description of the communication",$client->getResponse()->getContent());
+    }
+
+    public function testShortDescription()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/communication/submit');
+
+        $form = $crawler->selectButton('submit')->form();
+
+        //set form values
+        $form['form[description]']="Talked"; //description too short
+
+
+        $crawler = $client->submit($form);
+
+        assertContains("Please provide a description of 50 characters or more",$client->getResponse()->getContent());
+    }
+
+    public function testLongDescription()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/communication/submit');
+
+        $form = $crawler->selectButton('submit')->form();
+
+
+        //set form values
+        $form['form[description]']=str_repeat('a',2001);//generate a string that is too long
+
+
+        $crawler = $client->submit($form);
+
+        assertContains("Please keep the description under 2000 characters",$client->getResponse()->getContent());
+    }
+
+    //public function testDataBaseEntry()
+    //{
+    //    $client = static::createClient();
+
+    //    $crawler = $client->request('GET', '/communication/submit');
+
+    //    $form = $crawler->selectButton('submit')->form();
+
+    //    //set form values
+    //    $form['form[date]'] = "2017-10-05";
+    //    $form['form[type]']="Phone";
+    //    $form['form[medium]']="incoming";
+    //    $form['form[contact]']=1; //contact id
+    //    $form['form[property]']=1; //property id
+    //    $form['form[category]']="Container";
+    //    $form["form[description]"]="Container has graffiti and needs to be cleaned. Action request made";
+
+    //    $crawler = $client->submit($form);
+
+    //    $communicationRepo = $this->createMock(ObjectRepository::class);
+
+    //    $communicationRepo->expects($this->any())
+    //        ->method('find')
+    //        ->willReturn()
+
+    //}
 }
