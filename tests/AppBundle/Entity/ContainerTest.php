b2e02397390dd870f6e86db135a08cd0953548cd
Daniel Akre <akre7638@saskpolytech.ca>
2018-01-11 13:40:40 -0600

    Merging master into branch for updated tests <Dan Akre><Liam Johnson>

---
+++ tests/AppBundle/Entity/ContainerTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace tests\AppBundle\Entity; 
+
+use AppBundle\Entity\Container; 
+use PHPUnit\Framework\TestCase; 
+use Symfony\Component\Validator\Validation;
+
+class ContainerTest extends TestCase
+{
+    private $container; 
+    private $validator; 
+
+    protected function setUp()
+    {
+        
+    }
+}
+
