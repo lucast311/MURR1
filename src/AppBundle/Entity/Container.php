9a56785d440cada27964071eb55f5d80733780bc
Daniel Akre <akre7638@saskpolytech.ca>
2018-01-18 14:08:58 -0600

    Added error message to container form <dan Akre><Lian Johnson>

--- src/AppBundle/Entity/Container.php
+++ src/AppBundle/Entity/Container.php
@@ -4,12 +4,14 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Container
  *
  * @ORM\Table(name="container")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\ContainerRepository")
+ * @UniqueEntity(fields = {"containerSerial"}, message = "Serial already exists")
  */
 class Container
 {
