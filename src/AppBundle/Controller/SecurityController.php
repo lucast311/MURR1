4cc5971aa84a1897fe54e8305e30e6a9d2a2535e
Matthew Thompson <thompson7725@saskpolytech.ca>
2018-01-24 15:41:29 -0600

    Created code stub for the security controller. --Thompson pilot

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * This controller handles the logging in of users of the system
+ */
+class SecurityController extends Controller
+{
+    /**
+     * Story 15a
+     * Handles the login page
+     * 
+     * @param Request $request 
+     */
+    public function loginAction(Request $request)
+    {
+    }
+}
