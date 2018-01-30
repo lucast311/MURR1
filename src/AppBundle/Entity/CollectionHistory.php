d7352f416267780329edcbe7ed5dafdf1307eb5c
Liam Johnson <johnson1882@saskpolytech.ca>
2018-01-30 13:52:25 -0600

    Pilot: Liam Copilot: Dan Autogenerated some code using doctrine

---
+++ src/AppBundle/Entity/CollectionHistory.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CollectionHistory
+ *
+ * @ORM\Table(name="collection_history")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CollectionHistoryRepository")
+ */
+class CollectionHistory
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Container", inversedBy="ContainerSerial", cascade={"persist"})
+     * @ORM\JoinColumn(name="ContainerID", referencedColumnName="id")
+     */
+    private $containerId;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="notCollected", type="boolean", nullable=true)
+     */
+    private $notCollected;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="notes", type="string", length=250, nullable=true)
+     */
+    private $notes;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set containerId
+     *
+     * @param integer $containerId
+     *
+     * @return CollectionHistory
+     */
+    public function setContainerId($containerId)
+    {
+        $this->containerId = $containerId;
+
+        return $this;
+    }
+
+    /**
+     * Get containerId
+     *
+     * @return int
+     */
+    public function getContainerId()
+    {
+        return $this->containerId;
+    }
+
+    /**
+     * Set notCollected
+     *
+     * @param boolean $notCollected
+     *
+     * @return CollectionHistory
+     */
+    public function setNotCollected($notCollected)
+    {
+        $this->notCollected = $notCollected;
+
+        return $this;
+    }
+
+    /**
+     * Get notCollected
+     *
+     * @return bool
+     */
+    public function getNotCollected()
+    {
+        return $this->notCollected;
+    }
+
+    /**
+     * Set notes
+     *
+     * @param string $notes
+     *
+     * @return CollectionHistory
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    /**
+     * Get notes
+     *
+     * @return string
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+}
+
