b40d4cbbda577ea0eaf671329695ac97681a0600
Matthew Thompson <thompson7725@saskpolytech.ca>
2018-02-02 12:57:38 -0600

    Finished a unit test. Make the auto log in work better. --Thompson pilot

--- tests/Mink/PropertySearchPopupTest.php
+++ tests/Mink/PropertySearchPopupTest.php
@@ -11,40 +11,41 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class PropertySearchPopupTest extends WebTestCase
 {
     private $driver;
+    private $session;
 
     protected function setUp()
     {
         // Create a driver
         $this->driver = new ChromeDriver("http://localhost:9222",null, "localhost:8000");
         // Create a session and pass it the driver
-        //$session = new Session($this->driver);
-        /*
+        $this->session = new Session($this->driver);
+        
         //Log the user in
         // Start the session
-        $session->start();
+        $this->session->start();
 
         // go to the login page
-        $session->visit('http://localhost:8000/login');
+        $this->session->visit('http://localhost:8000/login');
         // Get the current page
-        $page = $session->getPage();
+        $page = $this->session->getPage();
         // Fill out the login form
         $page->findById("username")->setValue("admin");
         $page->findById("password")->setValue("password");
         // Submit the form
         $page->find('named', array('id_or_name', "login"))->submit();
-        */
+        
 
     }
 
     public function testCommunicationPropertyAdvancedSearch()
     {
         // Start up a new session
-        $session = new Session($this->driver);
-        $session->start();
+        //$this->session = new Session($this->driver);
+        //$this->session->start();
         // Navigate to the new communication page
-        $session->visit('http://localhost:8000/communication/new');
+        $this->session->visit('http://localhost:8000/communication/new');
         // Get the page
-        $page = $session->getPage();
+        $page = $this->session->getPage();
         // find and assert that there is an advanced search button
         $advancedSearchBtn = $page->find('named', array('button', "Advanced Search"));
         $this->assertNotNull($advancedSearchBtn);
@@ -56,7 +57,7 @@ class PropertySearchPopupTest extends WebTestCase
         // NOTE: may need to switch to popup window here, unsure how Mink handles the popup.
 
         // Assert that the search page information exists
-        $page = $session->getPage(); // May not need this
+        $page = $this->session->getPage(); // May not need this
         // Search box
         $this->assertNotNull($page->find('named', array('id', "searchBox")));
         // Table headers
@@ -71,6 +72,19 @@ class PropertySearchPopupTest extends WebTestCase
         // Search for a property
         $page->find('named', array('id', "searchBox"))->setValue("Charlton Arms");
 
-        // CONTINUE HERE. CLICK THE LINK.
+        // click the first link for one of the results
+        $page->find('named', array('content', "Select"))->click();
+
+        // May have to refresh the page, may not
+        $page = $this->session->getPage(); // May not need this
+
+        // Get the select box now and check that it has the right property in it
+        $this->assertEquals($page->find('named', array('id', "communication_property"))->getValue(), "Charlton Arms");
+    }
+
+    protected function tearDown()
+    {
+        // After the test has been run, make sure to restart the session so you don't run into problems
+        $this->session->restart();
     }
 }
