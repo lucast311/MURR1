f737c9b83334c9bedd49d61662fed1bd91dae7d5
Daniel Akre <akre7638@saskpolytech.ca>
2018-02-05 14:56:59 -0600

    Started adding functionality <Dan><Thompson>

--- web/JavaScript/PropertySearchPopup.js
+++ web/JavaScript/PropertySearchPopup.js
@@ -1,10 +1,12 @@
-﻿/**
+﻿
+
+/**
  * The onLoad function that loads the select2 library 
  */
 function onLoad()
 {
-
-
+    $("#communication_property").select2(); 
+    $("#communication_property").next().after("<input id='advanced_property_search_popup' value='Advanced Search' type='button'/>"); 
 }
 
 /**
@@ -14,3 +16,5 @@ function advancedSearch()
 {
 
 }
+
+$(onLoad);//when the page is done loading, run the onLoad function
