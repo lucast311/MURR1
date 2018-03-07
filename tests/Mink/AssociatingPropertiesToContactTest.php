67ca8bf236e01ce9e8d979e61c9f44cd5024e42e
Luke MacNeil <macneil4857@saskpolytech.ca>
2018-03-07 13:54:40 -0600

    Luke & Weeks - Finished frontend tests, continued to write functional tests.

--- tests/Mink/AssociatingPropertiesToContactTest.php
+++ tests/Mink/AssociatingPropertiesToContactTest.php
@@ -32,7 +32,7 @@ class AssociatingPropertiesToContactTest extends WebTestCase
         $contactLoader->load(DatabasePrimer::$em);
 
         $propertyLoader = new LoadPropertyData();
-        $propertyLoader->load(DatabasePrimer:$em);
+        $propertyLoader->load(DatabasePrimer::$em);
     }
 
     protected function setUp()
@@ -78,28 +78,108 @@ class AssociatingPropertiesToContactTest extends WebTestCase
      * Story 4k
      * Tests that you can use the simple search to find a value
      */
-    public function testAddAssociationSimpleSearch()
+    public function testAssociationSimpleSearch()
     {
         //start up a new session, going to the contact view page for Bill Jones (ID 24)
         $this->session->visit('http://localhost:8000/app_test.php/contact/24');
         // Get the page
         $page = $this->session->getPage();
 
-        $searchBox = $page->find("css","#propertySearch");
+        $searchBox = $page->find("css",".ui.search.dropdown input.search");
 
         //type charlton legs into the searchbox
-        $searchBox->setValue("Charlton Legs");
+        $searchBox->setValue("Balla Highrize");
 
         //Test that the filtered results contains charlton legs
-        $searchResults = $page->find("css","#searchResults");
-        $this->assertContains("Charlton Legs",$searchResults->getHtml());
+        $searchResults = $page->find("css",".ui.search.dropdown menu.transition.visible item.active.filtered");
+        $this->assertContains("Balla Highrize",$searchResults->getHtml());
 
         //only result shoudl be charlton legs, so click it
         $searchResults->click();
 
         //check that the form field contains the property charlton legs
-        $formField = $page->find("form[name='appbundle_propertyToContact'] input[name='property']");
-        $this->assertContains('Charlton Legs',$formField->getHtml());
+        $formField = $page->find("css", "form[name='appbundle_propertyToContact'] input[name='property']");
+        $this->assertContains('Balla Highrize',$formField->getValue());
+        $this->assertEquals(1, $formField->getValue());
+    }
+
+    /**
+     * Story 4k
+     * Tests all functionality related to the advanced property search on the contact view page.
+     * Ensures the button is there, that it functions correctly, and that the chosen property is set in the search box.
+     *
+     * HEY! This test will not run if popups are not allowed in chrome. GO ALLOW POPUPS IN CHROME.
+     */
+    public function testAssociationAdvancedSearch()
+    {
+        // Navigate to the contact view page
+        $this->session->visit('http://localhost:8000/app_test.php/contact/24');
+        // Get the page
+        $page = $this->session->getPage();
+
+        // find and assert that there is an advanced search button
+        $advancedSearchBtn = $page->find('named', array('button', "Advanced Search"));
+        $this->assertNotNull($advancedSearchBtn);
+        $this->assertEquals($advancedSearchBtn->getValue(), "Advanced Search");
+
+        // Get the names of all the windows
+        $windowNames = $this->session->getWindowNames();
+        // Take note of this current window so we can return to it later
+        $originalWindow = $this->session->getWindowName();
+
+        // Click on the advanced search button
+        $advancedSearchBtn->click();
+
+        // WAIT for the page to load, otherwise it will be empty when mink tries to use it.
+        $this->session->wait(1000);
+
+        // Get the names of all the windows AGAIN, so we can figure out which one is new
+        $newWindowNames = $this->session->getWindowNames();
+        // Figure out which is new. The only new window should be at position 0 in the resulting array
+        $popupWindowArray = array_diff($newWindowNames, $windowNames);
+
+        // Switch to the first window in the array (should be the popup)
+        $this->session->switchToWindow($popupWindowArray[0]);
+
+        // Re-get the page, since we are in a new window
+        $page = $this->session->getPage();
+
+        // Search box
+        $this->assertNotNull($page->find('named', array('id', "searchBox")));
+        // Table headers
+        $this->assertNotNull($page->find('named', array('content', "Site ID")));
+        $this->assertNotNull($page->find('named', array('content', "Property")));
+        $this->assertNotNull($page->find('named', array('content', "Type")));
+        $this->assertNotNull($page->find('named', array('content', "Status")));
+        $this->assertNotNull($page->find('named', array('content', "Structure Id")));
+        $this->assertNotNull($page->find('named', array('content', "Units")));
+        $this->assertNotNull($page->find('named', array('content', "Neighbourhood")));
+
+        // Search for a property
+        $page->find('named', array('id', "searchBox"))->setValue("Charlton Legs");
+
+        // Emulate a keyup to trigger the event that normally does a search.
+        // Don't know why, it doesn't matter what character you press as it doesn't seem to go in the box anyways.
+        $page->find('named', array('id', "searchBox"))->keyPress("s");
+
+        // Make Mink wait for the search to complete. This has to be REALLY long because the dev server is slow.
+        $this->session->wait(5000);
+
+        // click the first link for one of the results
+        $selectLink = $page->find('named', array('link', "Select"));
+        // Before we click the link, take the id of the property we clicked.
+        $id = $selectLink->getAttribute("data-id");
+        //Click the link
+        $selectLink->click();
+
+        // Switch back to the original window
+        $this->session->switchToWindow($originalWindow);
+
+        // Refresh the page content so it reflects the window
+        $page = $this->session->getPage();
+
+        // Get the select box now and check that it has the right property in it (based on the id of the property that was clicked originally)
+        $this->assertEquals($page->find('named', array('id', "propertySearch"))->getValue(), $id);
     }
 
     /**
