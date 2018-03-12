200d9948b9e94b8b2231fc11b9b7f6e2fe71fa3d
Matthew Thompson <thompson7725@saskpolytech.ca>
2018-03-12 14:01:34 -0600

    Worked on the delete functionality. --Thompson pilot, Nicholas copilot.

---
+++ web/JavaScript/DeleteModal.js
@@ -0,0 +1,23 @@
+﻿
+// Execute when the page loads
+var onLoad = function ()
+{
+    // Register all modals on the page with semantic
+    $('.ui.modal').modal();
+
+    // Register a click handler for the delete buttion
+    $("#delete").click(function ()
+    {
+        // show the modal
+        $('.ui.modal.delete').modal('show');
+    });
+
+     // Register an event handler to clicking the modal delete (the real deal delete)
+    $('ui negative right labeled icon button').click(function ()
+    {
+        $('div.ui.negative.labeled.icon.button form').submit();
+    });
+}
+
+// Call the onload function
+$(onLoad);
