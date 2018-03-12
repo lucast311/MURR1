8cd0fe512e80c17bb80b97b53690acb0cca1d118
William Weeks <weeks8056@saskpolytech.ca>
2018-03-12 13:51:49 -0600

    Adjusted modal code, wrote controller methods, just need error messages - Matt and Luke

--- web/JavaScript/RemoveModal.js
+++ web/JavaScript/RemoveModal.js
@@ -6,23 +6,38 @@
  */
 function showModal(message, formToSubmit)
 {
-    $(document.body).append(`
-<div class="ui basic modal" id="removeModal">
-    <i class="trash alternate outline icon"></i>
-    <p>` + message + `</p >
-    <button class="ui button inverted red" id="btnDecline"><i class="close icon"></i>No</button>
-    <button class="ui button inverted green"><i class="check icon" id="btnAccept"></i>Yes</button>
-</div>`);
-
+    //get the modal
     var modal = $('#removeModal');
 
+    //set the message in the modal
+    $("#removeModalMessage").html(message);
+
+    //show the modal
     modal.modal('show');
 
+    //clicking the decline button
     $('#btnDecline').click(function () {
         modal.modal('hide');
     });
 
+    //clicking the accept button
     $('#btnAccept').click(function () {
         modal.modal('hide');
+        //submit the form
+        form.submit();
     });
+}
+
+/**
+ * Adds a removal modal the the page
+ */
+function addModal()
+{
+    $(document.body).append(`
+    <div class="ui basic modal" id="removeModal">
+        <i class="trash alternate outline icon"></i>
+        <p id="removeModalMessage"></p>
+        <button class="ui button inverted" id="btnDecline"><i class="close icon"></i>Cancel</button>
+        <button class="ui button inverted red" id="btnAccept"><i class="check icon"></i>Remove</button>
+    </div>`);
 }
