ad7687a13d73c710465a77f9adc517f56341a809
Matthew Thompson <thompson7725@saskpolytech.ca>
2018-03-29 10:54:19 -0600

    Created test to add a truck to a route using the existing advanced search functionality. --Thompson pilot

--- tests/Mink/AddTruckToRouteTest.php
+++ tests/Mink/AddTruckToRouteTest.php
@@ -101,6 +101,89 @@ class AddTruckToRouteTest extends WebTestCase
 
     /**
      * Story 40b
+     * This test checks the functionality of adding a truck to a route that does not already have a truck assigned to it,
+     * But this time using the advanced search.
+     */
+    public function testAddTruckToRouteWithAdvancedSearch()
+    {
+        // Browse to the desired page
+        $this->session->visit('http://localhost:8000/app_test.php/route/1');
+        // Get the page
+        $page = $this->session->getPage();
+
+        // There should be no truck currently assigned, check for that
+        $this->assertContains($page->find('css', 'h1.ui.header.left.attached.top')->getHtml(), "No Truck Assigned");
+        // Assert that the assign truck form exists
+        $this->assertTrue($page->find('css', '#assignTruckForm')->isVisible());
+        // Make sure the change truck button is NOT visible
+        $this->assertFalse($page->find('named', array('button', 'Change Truck'))->isVisible());
+        // Make sure the unassign truck button is NOT visible
+        $this->assertFalse($page->find('named', array('button', 'Unassign Truck'))->isVisible());
+
+        // Fill out the truck form
+        // Get the names of all the windows
+        $windowNames = $this->session->getWindowNames();
+        // Take note of this current window so we can return to it later
+        $originalWindow = $this->session->getWindowName();
+
+        // Click on the advanced search button
+        $page->find('named', array('button', 'Advanced Search'))->click();
+
+        // WAIT for the page to load, otherwise it will be empty when mink tries to use it.
+        $this->session->wait(2000);
+
+        // Get the names of all the windows AGAIN, so we can figure out which one is new
+        $newWindowNames = $this->session->getWindowNames();
+        // Figure out which is new. The only new window should be at position 0 in the resulting array
+        $popupWindowArray = array_diff($newWindowNames, $windowNames);
+
+        // Switch to the first window in the array (seems to always be the popup)
+        $this->session->switchToWindow($popupWindowArray[0]);
+
+        // Re-get the page, since we are in a new window
+        $page = $this->session->getPage();
+
+        // Search for a truck
+        $page->find('named', array('id', "searchBox"))->setValue("000034");
+
+        // Emulate a keyup to trigger the event that normally does a search.
+        // Don't know why, it doesn't matter what character you press as it doesn't seem to go in the box anyways.
+        $page->find('named', array('id', "searchBox"))->keyPress("s");
+
+        // Make Mink wait for the search to complete. This has to be REALLY long because the dev server is slow.
+        $this->session->wait(5000);
+
+        // click the first row for one of the results
+        $selectLink = $page->find('css', 'table tbody tr:first-child');
+        // Before we click the link, take the id of the truck we clicked.
+        $id = $selectLink->getAttribute("data-id");
+        //Click the link
+        $selectLink->click();
+
+        // Switch back to the original window
+        $this->session->switchToWindow($originalWindow);
+
+        // Refresh the page content so it reflects the window
+        $page = $this->session->getPage();
+
+        // Assert the select box has the chosen truck in it
+        $this->assertEquals($page->find("css", "#truckDropdown")->getValue(), $id);
+
+        // Submit the truck form
+        $page->find('named', array('button', 'Assign'))->click();
+
+        // When the page reloads, assert that the truck is now assigned
+        $this->assertContains($page->find('css', 'h1.ui.header.left.attached.top')->getHtml(), "Truck 000034");
+        // Make sure the change truck button is visible
+        $this->assertTrue($page->find('named', array('button', 'Change Truck'))->isVisible());
+        // Make sure the unassign truck button is visible
+        $this->assertTrue($page->find('named', array('button', 'Unassign Truck'))->isVisible());
+        // Make sure the assign truck form is NOT visible
+        $this->assertFalse($page->find('css', '#assignTruckForm')->isVisible());
+    }
+
+    /**
+     * Story 40b
      * This test checks the functionality of changing a truck on a route that already has a truck assigned.
      */
     public function testChangeTruckOnRoute()
