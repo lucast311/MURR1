dcb9c829c65f554d4115ff2b066fe15364459535
William Weeks <weeks8056@saskpolytech.ca>
2018-04-03 14:57:18 -0600

    Finished front end styles and navigation menu. Started home page links - Matt Weeks, Liam Johnson

--- web/JavaScript/base_twig.js
+++ web/JavaScript/base_twig.js
@@ -2,19 +2,49 @@
     Simple functions that are loaded on every page.
 */
 $(function () {
-    // Everytime the #menuBtn button is clicked the sidebar will be toggled on or off.
-    $("#menuBtn").click(function () {
-        // Jquery selector for the sidebar
-        $('.ui.sidebar')
-            // Sets how the sidebar will be displayed
-            .sidebar('setting', 'transition', 'push')
-            .sidebar('toggle');
+
+    sideNav = $("#sideNav");
+
+    /*
+     * This is a callback function used whenever the page resizes to run javascript
+     */
+    function mediaQuery(x)
+    {
+        //javascript to run if the view is mobile
+        if (x.matches)
+        {
+            sideNav.removeClass("visible");
+        }
+        //javascript to run if the view is destkop
+        else
+        {
+            sideNav.addClass("visible");
+        }
+    }
+
+    //Specify that mobile is a window smaller than 767px
+    var x = window.matchMedia("(max-width: 767px)");
+    //call the function initially on page load
+    mediaQuery(x);
+    //add the media query listener for whenever the page resizes
+    x.addListener(mediaQuery);
+
+
+    //Specify a click handler on the hamburger button to open up the sidebar navigation
+    $("#openMenu").click(function () {
+        sideNav.sidebar('show');
     });
 
-    // selects all ui elements with the sticky class
-    // The sticky() function will have the stickied element follow the page
-    // until it hits the .footer element. (the bottom of the page)
-    $('.ui.sticky').sticky({ context: '.footer' });
+    //find the horizontal navigation bar
+    horizontalNav = $('horizontalNav');
+    //make it sticky based on the context of the page content
+    //Note that this only sticks on mobile
+    horizontalNav.sticky({ context: "#pageContent" });
+
+    //This will refresh the sticky any time the page content changes (because semantic event handlers aren't always smart enough)
+    $("#pageContent").bind("DOMSubtreeModified", function () {
+        horizontalNav.sticky("refresh");
+    });
 
     //This will enable radio buttons on any page to be clicked and selected (otherwise clicking them does nothing)
     $('.ui.checkbox').checkbox();
